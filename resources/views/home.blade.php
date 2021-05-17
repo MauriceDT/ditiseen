bde5853a875c8d24d72194ae74ae488a2317872f
Maurice di Tolve <maurice_di_tolve@hotmail.com>
2021-05-17 13:53:20 +0200

    17/05/2021 Start-up of project

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,12 @@
 @include('partials.header')
 
-<div class="home_center_div">
-    <h1 class="home_h1">This is Maurice' Magic Test Environment</h1>
+<div class="home_background_div">
+    <div class="home_center_div">
+        <h1 class="home_h1">This is Maurice' Magical Test Environment</h1>
+
+        <a href="/js_one"><button>js One</button></a>
+        <a href="/js_two"><button>js Two</button></a>
+    </div>
 </div>
 
 @include('partials.footer')
